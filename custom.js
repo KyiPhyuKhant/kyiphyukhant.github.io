7942b40d5c485525d172369baa92932319889afd
Kyi <khantkhant528@gmail.com>
2024-11-05 23:24:41 +0700

    updated for SEO

--- custom.js
+++ custom.js
@@ -1,3 +1,13 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   fetch('header.html')
+//     .then(response => response.text())
+//     .then(data => document.getElementById('header').innerHTML = data);
+
+//   fetch('footer.html')
+//     .then(response => response.text())
+//     .then(data => document.getElementById('footer').innerHTML = data);
+// });
+
 
 // Get the button
 const backToTopButton = document.getElementById('backToTop');
