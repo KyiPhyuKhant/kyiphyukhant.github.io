e1a1ad2ba84cbab447de3deaf98a54fdc1c19be5
Kyi <khantkhant528@gmail.com>
2024-11-05 16:14:44 +0700

    uploaded resume

--- custom.js
+++ custom.js
@@ -1,12 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const form = document.getElementById('contact-form');
-
-//   form.addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     alert('Thank you for your message! I will get back to you soon.');
-//     form.reset();
-//   });
-// });
 
 // Get the button
 const backToTopButton = document.getElementById('backToTop');
