0d55ae1e4f2ada8304a6226e30b638e0f82cddf7
Kyi <khantkhant528@gmail.com>
2024-11-05 16:07:56 +0700

    added and updated some sections

--- custom.js
+++ custom.js
@@ -1,12 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contact-form');
+// document.addEventListener('DOMContentLoaded', () => {
+//   const form = document.getElementById('contact-form');
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    alert('Thank you for your message! I will get back to you soon.');
-    form.reset();
-  });
-});
+//   form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+//     alert('Thank you for your message! I will get back to you soon.');
+//     form.reset();
+//   });
+// });
 
 // Get the button
 const backToTopButton = document.getElementById('backToTop');
@@ -29,30 +29,29 @@ backToTopButton.onclick = function () {
 };
 
 
-let currentSlide = 0;
+// let currentSlide = 0;
 
-function changeSlide(direction) {
-  const slides = document.querySelectorAll('.slide');
-  slides[currentSlide].classList.remove('active');
-  slides[currentSlide].style.opacity = '0'; // Start fading out
+// function changeSlide(direction) {
+//   const slides = document.querySelectorAll('.slide');
+//   slides[currentSlide].classList.remove('active');
+//   slides[currentSlide].style.opacity = '0'; // Start fading out
 
-  currentSlide = (currentSlide + direction + slides.length) % slides.length;
+//   currentSlide = (currentSlide + direction + slides.length) % slides.length;
 
-  slides[currentSlide].classList.add('active');
-  slides[currentSlide].style.opacity = '1'; // Start fading in
-}
+//   slides[currentSlide].classList.add('active');
+//   slides[currentSlide].style.opacity = '1'; // Start fading in
+// }
 
-// Initial setup
-document.querySelectorAll('.slide').forEach((slide, index) => {
-  if (index !== currentSlide) {
-    slide.style.display = 'none';
-    slide.style.opacity = '0'; // Set inactive slides to transparent
-  } else {
-    slide.style.display = 'block';
-    slide.style.opacity = '1'; // Active slide visible
-  }
-});
+// document.querySelectorAll('.slide').forEach((slide, index) => {
+//   if (index !== currentSlide) {
+//     slide.style.display = 'none';
+//     slide.style.opacity = '0'; // Set inactive slides to transparent
+//   } else {
+//     slide.style.display = 'block';
+//     slide.style.opacity = '1'; // Active slide visible
+//   }
+// });
 
-setInterval(() => {
-  changeSlide(1);
-}, 5000); // Automatically change slides every 5 seconds
+// setInterval(() => {
+//   changeSlide(1);
+// }, 5000); // Automatically change slides every 5 seconds
